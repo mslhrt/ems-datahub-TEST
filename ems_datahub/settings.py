79ae70362221479866dd5764bc80eea0b0a0e235
mslhrt <57634061+mslhrt@users.noreply.github.com>
2023-09-28 18:27:07 +0000

    Added ems_datahub.ems_dashboard to INSTALLED_APPS

--- ems_datahub/settings.py
+++ ems_datahub/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'ems_dashboard',
+    'ems_datahub.ems_dashboard',
 ]
 
 MIDDLEWARE = [
