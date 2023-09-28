028a95a77cdb89d2b5d38a52adccf2cdc1ac00dc
mslhrt <57634061+mslhrt@users.noreply.github.com>
2023-09-28 18:05:14 +0000

    changed ems_dashboard to ems-dashboard

--- ems-datahub/ems-dashboard/apps.py
+++ ems-datahub/ems-dashboard/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class EmsDashboardConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'ems_dashboard'
+    name = 'ems-dashboard'
