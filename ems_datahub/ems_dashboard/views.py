5f68aa507aa6271d85e3ad5b432d0788055698b3
mslhrt <57634061+mslhrt@users.noreply.github.com>
2023-09-29 16:34:44 +0000

    Fixing add_call function

--- ems_datahub/ems_dashboard/views.py
+++ ems_datahub/ems_dashboard/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import Call
 from .forms import CallForm
+from django.urls import reverse
 
 def list_calls(request):
     calls = Call.objects.all()
@@ -11,7 +12,7 @@ def add_call(request):
         form = CallForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('list_calls')
+            return redirect(reverse('ems_dashboard:list_calls'))  # Use reverse to get the URL
     else:
         form = CallForm()
     return render(request, 'ems_dashboard/add_call.html', {'form': form})
