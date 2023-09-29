74836dadea74c82222b8ff8d7d6432b9f4d78e1d
mslhrt <57634061+mslhrt@users.noreply.github.com>
2023-09-29 18:40:46 +0000

    Add Delete Feature

--- ems_datahub/ems_dashboard/urls.py
+++ ems_datahub/ems_dashboard/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import list_calls, add_call, CallUpdateView  # Import views
+from .views import list_calls, add_call, CallUpdateView, CallDeleteView    # Import views
 
 app_name = 'ems_dashboard'
 
 urlpatterns = [
-    path('list/', list_calls, name='list_calls'), #List Calls Page
-    path('add/', add_call, name='add_call'), #Add Call Page
-    path('edit/<int:pk>/', CallUpdateView.as_view(), name='edit_call'),  # Edit Calls Page
+    path('list/', list_calls, name='list_calls'), #list_calls View
+    path('add/', add_call, name='add_call'), #add_call View
+    path('edit/<int:pk>/', CallUpdateView.as_view(), name='edit_call'),  # Update View
+    path('delete/<int:pk>/', CallDeleteView.as_view(), name='delete_call'),  # Delete View
 ]
--- ems_datahub/ems_dashboard/views.py
+++ ems_datahub/ems_dashboard/views.py
@@ -4,6 +4,7 @@ from .forms import CallForm
 from django.urls import reverse
 from django.urls import reverse_lazy
 from django.views.generic.edit import UpdateView
+from django.views.generic.edit import DeleteView
 
 def list_calls(request):
     calls = Call.objects.all().order_by('-date', '-time')  # Order by date and time in descending order
@@ -23,6 +24,13 @@ class CallUpdateView(UpdateView):
     model = Call
     form_class = CallForm
     template_name = 'ems_dashboard/edit_call.html'
+    
+    def get_success_url(self):
+        return reverse_lazy('ems_dashboard:list_calls')
+
+class CallDeleteView(DeleteView):
+    model = Call
+    template_name = 'ems_dashboard/delete_call.html'
     
     def get_success_url(self):
         return reverse_lazy('ems_dashboard:list_calls')
