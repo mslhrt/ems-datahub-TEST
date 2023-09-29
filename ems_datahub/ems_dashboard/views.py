bf010b3d3e2a852e49bd910741c8105c9867db9f
mslhrt <57634061+mslhrt@users.noreply.github.com>
2023-09-29 14:54:20 +0000

    Views - first pass

--- ems_datahub/ems_dashboard/views.py
+++ ems_datahub/ems_dashboard/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import CallData  # Import other models as needed
 
-# Create your views here.
+def dashboard(request):
+    call_data = CallData.objects.all()  # Retrieve all call data from the database
+    context = {'call_data': call_data}
+    return render(request, 'ems_dashboard/dashboard.html', context)
