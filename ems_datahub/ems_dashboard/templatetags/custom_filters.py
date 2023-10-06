5521e70ead77a989bb6b4f2a6d45e7482279a2c5
mslhrt <57634061+mslhrt@users.noreply.github.com>
2023-10-06 18:09:48 +0000

    Adding custom filter for Query Database display permissions

---
+++ ems_datahub/ems_dashboard/templatetags/custom_filters.py
@@ -0,0 +1,11 @@
+from django import template
+from django.contrib.auth.models import Group
+
+register = template.Library()
+
+@register.filter(name='has_group')
+def has_group(user, group_name):
+    """
+    Check if a user is part of a specific group.
+    """
+    return Group.objects.get(name=group_name).user_set.filter(id=user.id).exists()
