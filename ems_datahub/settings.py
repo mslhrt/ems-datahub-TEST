a71352b4443f9a84a26a4daef4ba77c5a7557125
mslhrt <57634061+mslhrt@users.noreply.github.com>
2023-10-06 14:03:46 +0000

    Adding whitenoise to get dashboard to work

--- ems_datahub/settings.py
+++ ems_datahub/settings.py
@@ -128,3 +128,12 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'  # Add this line to specify the directory
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MIDDLEWARE = [
+    # ...
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    # ...
+]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
