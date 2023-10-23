87f9b232679978ec079feee7c2127c807b444ba2
mslhrt <57634061+mslhrt@users.noreply.github.com>
2023-10-23 17:11:08 +0000

    Add data validation check to ensure date/time is not in the future

--- ems_datahub/ems_dashboard/forms.py
+++ ems_datahub/ems_dashboard/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from datetime import datetime
 from .models import Call
 
 class CallForm(forms.ModelForm):
@@ -9,6 +10,18 @@ class CallForm(forms.ModelForm):
             'date': forms.DateInput(attrs={'type': 'date'}),
             'time': forms.TimeInput(attrs={'type': 'time'}),
         }
+
+    def clean(self):
+        cleaned_data = super().clean()
+        input_date = cleaned_data.get("date")
+        input_time = cleaned_data.get("time")
+        now = datetime.now()
+        input_datetime = datetime.combine(input_date, input_time)
+
+        if input_datetime > now:
+            raise forms.ValidationError("The date/time cannot be in the future.")
+
+        return cleaned_data
 
 class DataImportForm(forms.Form):
     data_file = forms.FileField(label='Select a file')
